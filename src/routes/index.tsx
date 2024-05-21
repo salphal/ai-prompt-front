7d70e424d953c277b9248f22d85316434c071af2
chenliang <>
2024-05-21 16:53:18 +0800

    feat: ✨ add edit json and prompt to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,9 @@
 import {Navigate, type RouteObject} from 'react-router-dom'
 import OverviewRoutes from "@/pages/overview/route.tsx";
-import HomeRoutes from "@/pages/home/route.tsx";
 import Test from "@/pages/test.tsx";
+import HomeRoutes from "@/pages/home/route.tsx";
+import EditPromptRoutes from "@/pages/edit-prompt/route.tsx";
+import EditJsonRoutes from "@/pages/edit-json/route.tsx";
 
 const routes: RouteObject[] = [
   {
@@ -15,6 +17,14 @@ const routes: RouteObject[] = [
   {
     path: '/home',
     element: <HomeRoutes/>
+  },
+  {
+    path: '/edit-prompt',
+    element: <EditPromptRoutes/>
+  },
+  {
+    path: '/edit-json',
+    element: <EditJsonRoutes/>
   },
   {
     path: '/test',
