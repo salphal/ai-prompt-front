296363848893415d072a47ffbf26c3601d7d6e8e
chenliang <>
2024-06-05 16:51:07 +0800

    fix: 🐛 turn off lints

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,18 +5,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'prettier',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-unused-vars": 0,
-    "no-empty-pattern": 0,
-    "no-unused-vars": 0
+    'react-refresh/only-export-components': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'no-empty-pattern': 0,
+    'no-unused-vars': 0,
+    'react-refresh/no-refs': 0,
+    'react-hooks/exhaustive-deps': 0,
   },
-}
+};
