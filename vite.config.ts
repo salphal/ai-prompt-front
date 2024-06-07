672b3b55cbece1bc03760dd003ce12718c224926
chenliang <>
2024-06-07 14:49:06 +0800

    feat: ✨ add terser

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': '/src',
     },
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
