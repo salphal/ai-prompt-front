81000503bc09e9a03394328a25a160f9e67ee40d
chenliang <>
2024-06-12 16:30:14 +0800

    fix: 🐛 remove unused code

--- src/pages/overview/overview.tsx
+++ src/pages/overview/overview.tsx
@@ -12,41 +12,11 @@ export interface OverviewProps {
 }
 
 const Overview: React.FC<OverviewProps> = (props: OverviewProps) => {
-  // const [content, setContent] = useState({
-  //   json: {
-  //     name: "Hello World",
-  //   },
-  //   text: undefined
-  // });
-  //
-  // const [dataList, setDataList] = useState([
-  //   {
-  //     render: <PromptMessage data={{role: 'user', content: 'first'}}/>,
-  //   },
-  //   {
-  //     render: <PromptMessage data={{role: 'system', content: 'second'}}/>,
-  //   },
-  //   {
-  //     render: <PromptMessage data={{role: 'assistant', content: 'third'}}/>,
-  //   }
-  // ]);
-
   useEffect(() => {}, []);
 
   return (
     <React.Fragment>
       <div className={classNames([Styles.overview])}>
-        {/*<DraggableList dataList={dataList} setDataList={setDataList}/>*/}
-        {/*<PromptForm/>*/}
-        {/*<JsonEditor*/}
-        {/*  mode={'text'}*/}
-        {/*  mainMenuBar={false}*/}
-        {/*  statusBar={false}*/}
-        {/*  content={content}*/}
-        {/*  onChange={setContent}*/}
-        {/*/>*/}
-        {/*<PromptMessage/>*/}
-
         <div className={Styles.info}>
           <div className={Styles.title}>Pick a prompt word</div>
           <div className={Styles.subTitle}>
