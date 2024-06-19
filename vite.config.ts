61ccee080f34d8df4ffd31f355624e0cafe92e16
chen <92xiaodao@gmail.com>
2024-06-19 18:01:44 +0800

    feat: ✨ update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react';
-import { ConfigEnv, defineConfig, UserConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite';
 
 /**
  * https://vitejs.dev/config/
@@ -8,12 +8,30 @@ import { ConfigEnv, defineConfig, UserConfig } from 'vite';
  * @param mode {string} - 环境
  */
 export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  console.log('=>(vite.config.ts:11) mode', mode);
+  console.log('=>(vite.config.ts:11) command', command);
+  console.log('=>(vite.config.ts:11) command', process.cwd());
+
+  const root = process.cwd(); // 根目录
+  /**
+   * @param mode {string} - 环境
+   * @param root {string} - 目录
+   * @param prefix {string} - 仅读取该前缀的环境变量
+   *
+   * @return {[key: string]: string} - 返回的值类型都是字符串
+   */
+  const envs = loadEnv(mode, root, 'VITE_');
+  console.log('=>(vite.config.ts:22) envs', envs);
+
   return {
+    server: {
+      port: 3000,
+      open: true,
+    },
     plugins: [react()],
     resolve: {
       alias: {
         '@': '/src',
-        '@components': '/src/components',
       },
     },
     build: {
