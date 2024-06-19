d49401037740a5178839eeb79c88d78599942f7f
chen <92xiaodao@gmail.com>
2024-06-19 17:16:26 +0800

    feat: ✨ update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
+/**
+ * https://vitejs.dev/config/
+ *
+ * @param command {string} - 执行的命令
+ * @param mode {string} - 环境
+ */
+export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components',
       },
     },
-  },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+    },
+  };
 });
